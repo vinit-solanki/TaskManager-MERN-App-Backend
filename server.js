2b120b3f597d5ab5b6942d6d0cbf30bfca5b3507
Vinit Solanki <2022.vinit.solanki@ves.ac.in>
2025-11-27 13:12:15 +0530

    fix: again added app.listen

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
